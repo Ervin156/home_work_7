8658ac42e53ed5c0bbadcf5dd34817b494393526
Your Name <you@example.com>
2019-09-20 10:41:36 +0300

    finish

--- main.js
+++ main.js
@@ -1,89 +1,85 @@
 ﻿var userManager = {
+  data: ["Beavis", "Butthead", "Cartman", "Mark", "Bob"],
 
-    data: ['Beavis', 'Butthead', 'Cartman', 'Mark', 'Bob',],
+  btnFind: document.createElement("button"),
+  btnList: document.createElement("button"),
+  btnClear: document.createElement("button"),
 
-    btnFind: document.createElement('button'),
-    btnList: document.createElement('button'),
-    btnClear: document.createElement('button'),
+  findUser: function(names, string) {
+    var li = document.createElement("li");
+    var span = document.createElement("span");
 
-    findUser: function (names, string) {
-        var li = document.createElement('li');
-        var span = document.createElement('span');
+    if (names.indexOf(string.value) !== -1) {
+      setTimeout(() => {
+        span.innerHTML += "Имя есть в списке";
+        blockAlert.append(span);
+        string.value = "";
+      }, 0);
+      return;
+    } else {
+      setTimeout(() => {
+        span.innerHTML += "Имя добавлено";
+        blockAlert.append(span);
+        names.push(string.value);
+        li.innerHTML += string.value;
+        ul.appendChild(li);
+        string.value = "";
+        return;
+      }, 0);
+    }
+  },
+  listUser: function(names, string) {
+    ul.style.listStyle = "none";
+    for (var i of names) {
+      var li = document.createElement("li");
+      li.innerHTML = i;
+      ul.append(li);
+    }
+    block.append(ul);
+  },
+  clearBlock: function(string) {
+    ul.innerHTML = "";
+    string.value = "";
+  },
+  init: function(data, inputName) {
+    body = document.querySelector("body");
+    block = document.createElement("div");
+    blockInput = document.createElement("div");
+    blockButtons = document.createElement("div");
+    blockAlert = document.createElement("div");
+    inputName = document.createElement("input");
+    ul = document.createElement("ul");
 
-        if (names.indexOf(string.value) !== -1) {
-            setTimeout(() => {
-                span.innerHTML += 'Имя есть в списке';
-                blockAlert.append(span);
-                string.value = '';
-            }, 0);
-            return;
-        } else {
-            setTimeout(() => {
-                span.innerHTML += 'Имя добавлено';
-                blockAlert.append(span);
-                li.innerHTML += string.value;
-                ul.appendChild(li);
-                names.push(string.value);
-                string.value = '';
-                return;
-            }, 0)
-        };
+    blockButtons.append(this.btnFind);
+    blockButtons.append(this.btnList);
+    blockButtons.append(this.btnClear);
 
-    },
-    listUser: function (names, string) {
-        ul.style.listStyle = 'none';
-        ul.innerHTML = '';
+    this.btnFind.textContent = "Find";
+    this.btnList.textContent = "Show List";
+    this.btnClear.textContent = "Clear";
+
+    blockInput.append(inputName);
+    blockInput.append(block);
+
+    block.appendChild(blockAlert);
+    body.append(blockButtons);
+    body.append(blockInput);
+
+    this.btnFind.onclick = () => {
+      blockAlert.innerHTML = "";
+      this.findUser(data, inputName);
+    };
+
+    this.btnList.onclick = () => {
+        this.clearBlock(inputName);
+
         setTimeout(() => {
-            for (var i of names) {
-                var li = document.createElement('li');
-                li.innerHTML = i;
-                ul.appendChild(li);
-            }
-            string.value = '';
-            block.append(ul);
-        }, 100);
-    },
-    clearBlock: function (string) {
-        ul.innerHTML = '';
-        string.value = '';
-    },
-    init: function (data, inputName) {
-
-        body = document.querySelector('body');
-        block = document.createElement('div');
-        blockInput = document.createElement('div');
-        blockButtons = document.createElement('div');
-        blockAlert = document.createElement('div');
-        inputName = document.createElement('input');
-        ul = document.createElement('ul');
-
-        blockButtons.append(this.btnFind);
-        blockButtons.append(this.btnList);
-        blockButtons.append(this.btnClear);
-
-        this.btnFind.textContent = 'Find';
-        this.btnList.textContent = 'Show List';
-        this.btnClear.textContent = 'Clear';
-
-        blockInput.append(inputName);
-        blockInput.append(block);
-
-        block.appendChild(blockAlert);
-        body.append(blockButtons);
-        body.append(blockInput);
-
-        this.btnFind.onclick = () => {
-            blockAlert.innerHTML = '';
-            this.findUser(data, inputName);
-        };
-
-        this.btnList.onclick = () => {
             this.listUser(data, inputName);
-        };
+          }, 100);    };
 
-        this.btnClear.onclick = () => {
-            this.clearBlock(inputName);
-        };
-    },
+    this.btnClear.onclick = () => {
+      this.clearBlock(inputName);
+    };
+  }
 };
 userManager.init(userManager.data, userManager.inputName);
